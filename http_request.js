f3c1ffba678eb614cb1cd66bfa9f07a5497bf83c
Vallentin <mail@vallentinsource.com>
2016-03-12 01:53:19 +0100

    Fixed name

--- http_request.js
+++ http_request.js
@@ -1,5 +1,5 @@
 
-// Author: Vallentin <mail@vallentinsource.com>
+// Author: Christian Vallentin <mail@vallentinsource.com>
 // Website: http://vallentinsource.com
 // Repository: https://github.com/MrVallentin/http_request
 //
